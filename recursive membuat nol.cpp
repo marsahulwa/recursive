e3583f80feb1a7bd4a5ca3e8d105b8d8e6f8ff9f
marsahulwa <99006063+marsahulwa@users.noreply.github.com>
2022-06-08 11:51:02 +0700

    Add files via upload

---
+++ recursive membuat nol.cpp
@@ -0,0 +1,29 @@
+//Nama :Ioshi Junika Fandha
+//NPM : G1A021026
+//Kelas : Informatika B
+
+#include<iostream>
+using namespace std;
+int zero(int angka)
+{
+static int hitung=0;
+    if(angka>0)
+    {
+
+        if(angka%10==0)
+        hitung++;
+
+        zero(angka/10);
+    }
+    return hitung;
+}
+int main()
+{
+    int n;
+    cout<<"---------Menghitung Jumlah Angka Nol---------"<<endl;
+    cout<<"---------------------------------------------"<<endl;
+    cout<<"Masukkan angka :";
+    cin>>n;
+    cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+    cout<<"Banyak angka nol : "<<zero(n);
+}
